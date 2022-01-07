94c6038c28fc0106905bb05db79154c603c8737c
hyeonho.joo <sparrow9576@gmail.com>
2022-01-08 06:37:01 +0900

    Add:hyeonho_module

--- scripts/modal.js
+++ scripts/modal.js
@@ -2,11 +2,11 @@
 const btn = document.querySelector('.btn-select');
 const list = document.querySelector('.list-member');
 btn.addEventListener('click', () => {
-    btn.classList.toggle('on');
+  btn.classList.toggle('on');
 });
 list.addEventListener('click', (event) => {
-    if (event.target.nodeName === "BUTTON") {
-        btn.innerText = event.target.innerText;
-        btn.classList.remove('on');
-    }
+  if (event.target.nodeName === "BUTTON") {
+    btn.innerText = event.target.innerText;
+    btn.classList.remove('on');
+  }
 });
