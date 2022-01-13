e6473353cad43774a2a8cbeb125e42a09a1957c2
hyeonho.joo <sparrow9576@gmail.com>
2022-01-14 01:57:06 +0900

    Add : home-feed API 연동

--- scripts/4.home.js
+++ scripts/4.home.js
@@ -7,6 +7,7 @@ const container = document.querySelector('.container');
     else{
         location.href = './2.login.html'
     }
+
     async function getFeed() {
         const token = localStorage.getItem("Token")
         const res = await fetch(API_URL+"/post/feed",{
@@ -25,19 +26,54 @@ const container = document.querySelector('.container');
             const authorName = post.author.username
             const commentCount = post.commentCount
             const content = post.content
+            const image = post.image
             const heartCount = post.heartCount
             const hearted = post.hearted
+
+            // for(let post of json.post){
+            //     const imageArr = post.image.split(',')
+            //     let images = ''
+            //     for (let image of imageArr){
+            //       images += `<img src=${image} alt="피드 이미지" class="img-feed">`
+            //     }
+            
+
             document.querySelector(".container").innerHTML+=`
-            <div class="post-container">
-                <img class="profileimg" src="${authorImage}"/>
-                <div class="h">${authorAccount}</div>
-                <div class="h">${authorName}</div>
-                <div class="h">${content}</div>
-                <div class="h">${commentCount}</div>
-                <div class="${hearted ?"yes":"no"}">${hearted}</div>
-                
-            </div>                
-            `
+            <section class="feed-cont has-cont">
+              <h2 class="txt-hide">팔로워 피드</h2>
+                <article class="home-post">
+                    <h5 class="txt-hide">피드 게시글</h5>
+                    <ul class="wrap-profile">
+                    <li>
+                        <img src=${post.author.image} alt="기본프로필 소형" class="basic-profile">
+                    </li>
+                    <li>
+                        <ul class="wrap-right">
+                        <li class="user-name">${post.author.username}</li>
+                        <li class="user-id">@ ${post.author.accountname}</li>
+                        </ul>
+                    </li>
+                    </ul>
+                <div class="main-feed">
+                    <p class="txt-feed">${post.content}</p>
+                    <img src=${post.image} alt="피드 이미지" class="img-feed">
+                    <ul class="wrap-reaction">
+                        <li class="wrap-like-btn">
+                        <button type="button"><img src="../images/icon/icon-heart.png" alt="빈 하트 아이콘" class="icon-heart"></button>
+                        <span class="like-count">${post.heartCount}</span>
+                        </li>
+                        <li>
+                        <a href="#10.html">
+                            <img src="../images/icon/icon-message-circle.png" alt="댓글 이미지" class="chat-icon-message-circle">
+                        </a>
+                            <span>12</span>
+                        </li>
+                    </ul>
+                    <small class="txt-date">2020년 10월 21일</small>
+                </div>
+            <article>
+        </section>
+        `
         });
     }
     getFeed()
