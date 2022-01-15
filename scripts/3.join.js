392fc58ce8718aabb218b1af6efbbd9a66c4ed4a
sooonlog <93389773+sooonlog@users.noreply.github.com>
2022-01-15 21:45:41 +0900

    🎨 이메일로 회원가입 기능 구현

--- scripts/3.join.js
+++ scripts/3.join.js
@@ -1,51 +1,55 @@
 // join_email.html
 const joinEmailInput = document.querySelector("#join-cont-email");
 const joinPwdInput = document.querySelector("#join-cont-pwd");
-const joinBtnNext = document.querySelector("#join-next");
-
-console.log(joinEmailInput);
+const joinBtnNext = document.querySelector(".join-next");
+const emailWarnTxt = document.querySelector(".login-email-warn");
+const pwdWarnTxt = document.querySelector(".login-pwd-warn");
 
 // 포커스를 잃었을 때 유효성 검사
-joinEmailInput.addEventListener('focusout', () => {
-  // console.log("email focus out");
-  // 유효성 검사하는 코드 넣기
+joinEmailInput.addEventListener('focusout', async () => {
+  const email = joinEmailInput.value;
+  const pw = joinPwdInput.value;
+    const validEmail = await checkEmailValid(email);
+    if(validEmail) {
+      emailWarnTxt.classList.remove('txt-hide','login-warn');
+      emailWarnTxt.innerText = "* 사용 가능한 이메일 입니다.";
+    }
+    else {
+      emailWarnTxt.innerText = "* 이미 가입된 이메일 주소입니다.";
+      emailWarnTxt.classList.remove('txt-hide');
+      emailWarnTxt.classList.add('on');
+    }
 });
 
-// 이미 가입된 이메일인 경우 경고
-function getInput() {
-  console.log(document.querySelector("#join-cont-email").value);
-  console.log(document.querySelector("#join-cont-pwd").value);
-};
-
-// 서버에서 기존 유저 이메일 가져오기
-async function getUserData() {
-  const response = await fetch(`http://146.56.183.55:5050/user`);
-  this.userData = await response.json();
-};
-
-console.log(getUserData());
-// getUserData();
-
-joinEmailInput.addEventListener('focusout', () => {
-  // getInput();
-  if(joinEmailInput.value === ''){
-
+joinPwdInput.addEventListener('focusout', async () => {
+  const pw = joinPwdInput.value;
+  if(pw === '') {
+    pwdWarnTxt.classList.remove('txt-hide');
+    pwdWarnTxt.classList.add('on');
+  } else if(pw.length>5) {
+    joinBtnNext.classList.remove('disabled');
+    joinBtnNext.disabled = false;
+    pwdWarnTxt.classList.add('txt-hide');
+  } else {
+    pwdWarnTxt.innerText = "* 비밀번호는 6자 이상이어야 합니다.";
+    pwdWarnTxt.classList.remove('txt-hide');
+    pwdWarnTxt.classList.add('on');
   }
 });
 
-
-
-// 비밀번호가 6자 미만인 경우 경고
-
-
-// 유효성 검사를 통과할 경우 다음 버튼 활성화
-
-
-// 다음 버튼 클릭 시 프로필 설정으로 이동
-
-
-// 계정 ID 유효성 검사
-
-
-// 계정 ID 중복 검사
-
+// 이메일 중복체크
+async function checkEmailValid(email) {
+  const res = await fetch(`http://146.56.183.55:5050/user/emailvalid`, {
+    method:"POST",
+    headers: {
+            "Content-Type": "application/json",
+        },
+    body:JSON.stringify({
+        "user":{
+                "email":email
+        }
+    })
+  });
+  const json = await res.json();
+  return json.message == "사용 가능한 이메일 입니다." ? true : false
+}
