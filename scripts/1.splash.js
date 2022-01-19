59d2f9b3bfafa616dd5e1eeeae2602bc4c605c0d
sooonlog <93389773+sooonlog@users.noreply.github.com>
2022-01-19 17:32:49 +0900

    ⚡️ Update: 토큰여부에 따라 다른 페이지 이동 기능 구현

--- scripts/1.splash.js
+++ scripts/1.splash.js
@@ -1,4 +1,13 @@
-function splashpage() {
-  window.location.href = '../pages/2.login.html'
-}
+// 시작 화면
+// index.html 화면이 로드되고 2초 뒤 로그인 화면으로 이동
 window.onload = setTimeout(splashpage, 2000);
+
+// 토큰이 있는 경우 홈피드로 이동
+function splashpage() {
+  const token = localStorage.getItem("Token");
+  if(token != '') {
+    location.href = './4.home.html'
+  } else {
+    location.href = './2.login.html'
+  }
+}
