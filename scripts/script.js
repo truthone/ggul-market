4414ede81cbee2cb453bf08b851da5045fc7a729
jiseung-kang <ill0o0lli@ajou.ac.kr>
2022-01-19 13:06:17 +0900

    ⚡️ Update : 피드 이미지 좌우스크롤 이벤트 업데이트

--- scripts/script.js
+++ scripts/script.js
@@ -3,25 +3,30 @@ const API_URL = "http://146.56.183.55:5050"
 function loadPost(idx, post, imageArr, imageLength, isMyprofile, authorName) {
   const goURL = `6.profile.html?${authorName}`
 
-  let btnMsg = isMyprofile?'modal-my-edit':'modal-other-edit';
+  let btnMsg = isMyprofile ? 'modal-my-edit' : 'modal-other-edit';
 
   let images = ''
   let list = document.createElement('article')
   list.classList = 'home-post'
-  
+
   let date = post.createdAt.slice(0, 10).split('-')
   // 이미지가 있을 때
-  if (post.image){
-    for (let image of imageArr){
+  if (post.image) {
+    for (let image of imageArr) {
       images += `<div><img src=${image} alt="피드 이미지" class="img-feed"></div>`
     }
     // 다중 이미지 처리
-    if (imageLength > 1){
-      images = `<div class="wrap-images" id="wrap-images${idx}">${images}</div><button type="button" class="btn-left" id="btn-left${idx}">⬅️</button><button type="button" class="btn-right" id="btn-right${idx}">➡️</button>`;
+    if (imageLength > 1) {
+      let btn = '';
+      for (let i = 0; i < imageLength; i++) {
+        btn += `<button type="button" class="btn-image btn-image${idx}" id="btn${idx}-${i}"></button>`
+      }
+      btn = `<div class="wrap-btn">${btn}</div>`
+      images = `<div class="wrap-images" id="wrap-images${idx}">${images}</div>${btn}`;
     }
   }
   // console.log(post.hearted)
-  let heartimage = post.hearted?'<img src="../images/icon/icon-heart-active.png" alt="좋아요 이미지" class="icon-heart">':'<img src="../images/icon/icon-heart.png" alt="좋아요 이미지" class="icon-heart">'
+  let heartimage = post.hearted ? '<img src="../images/icon/icon-heart-active.png" alt="좋아요 이미지" class="icon-heart">' : '<img src="../images/icon/icon-heart.png" alt="좋아요 이미지" class="icon-heart">'
 
   list.innerHTML = `<h5 class="txt-hide">피드 게시글</h5>
   <ul class="wrap-profile">
@@ -58,67 +63,33 @@ function loadPost(idx, post, imageArr, imageLength, isMyprofile, authorName) {
 
 // 다중 이미지 슬라이드(스크롤)
 function handleImageScroll(idx, imageLength) {
-  let btnLeft = document.querySelector(`#btn-left${idx-1}`)
-  let btnRight = document.querySelector(`#btn-right${idx-1}`)
+  let btnImage = document.querySelectorAll(`.btn-image${idx-1}`)
   let wrapImages = document.querySelector(`#wrap-images${idx-1}`)
   let imageSize = wrapImages.getBoundingClientRect().width;
-
   let pos = 0 // 현재 보고 있는 이미지
-  btnShow()
 
+  btnImage.forEach(btn => {
+    btn.addEventListener('click', () => {
+      pos = btn.id[btn.id.length - 1]
+      wrapImages.scroll({
+        left: (imageSize * pos),
+        behavior: 'smooth'
+      })
+    })
+  });
   let scrollLeft;
-  let totalWidth = wrapImages.clientWidth * (imageLength - 1)
-
   // 스크롤 동작
   wrapImages.addEventListener('scroll', () => {
     scrollLeft = wrapImages.scrollLeft;
-    pos = scrollLeft/wrapImages.clientWidth
-    if(scrollLeft == 0) {
-      btnShow()
-    }
-    else if(scrollLeft > totalWidth) {
-      btnShow()
-    }
-    else {
-      btnShow()
-    }
-  })
-
-  // 버튼 보이고 숨기기
-  function btnShow() {
+    pos = scrollLeft / wrapImages.clientWidth
     pos = Math.round(pos)
-
-    if (pos == 0){
-      btnLeft.style.display = "none";
+    console.log(pos, btnImage[pos])
+    for (let i = 0; i < imageLength; i++) {
+      if (i == pos) {
+        btnImage[i].style.background = "orange";
+      } else {
+        btnImage[i].style.background = "lightgray";
+      }
     }
-    else if (pos == imageLength - 1){
-      btnRight.style.display = "none";
-    }
-    else {
-      btnLeft.style.display = "block";
-      btnRight.style.display = "block";
-    }
-  }
-
-  // 버튼 클릭 이벤트
-  btnLeft.addEventListener('click', () => {
-    if (pos >= 1) {
-      pos -= 1;
-    }
-    wrapImages.scroll({
-      left: (imageSize*pos),
-      behavior: 'smooth'
-    })
-    btnShow()
-  })
-  btnRight.addEventListener('click', () => {
-    if (pos < imageLength - 1) {
-      pos += 1;
-    }
-    wrapImages.scroll({
-      left: (imageSize*pos),
-      behavior: 'smooth'
-    })
-    btnShow()
   })
 }
